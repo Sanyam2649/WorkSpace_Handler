2fb3cade26df0420cbd32c3248b3070bf10e5b94
Sanyam2649 <sb.sanyambansal2003@gmail.com>
2025-10-18 22:37:02 +0530

    Code mailer

--- Backend/services/nodeMailer.js
+++ Backend/services/nodeMailer.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 // Create transporter
 const transporter = nodemailer.createTransport({
   service: "gmail",
+   port: 587,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
